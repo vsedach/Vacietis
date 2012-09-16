893316cfb5ccb891af175de5d79608f953e8c0a0
Brit Butler <redline6561@gmail.com>
2012-09-16 14:13:43 -0400

    Add trivial-dump-core for executables on CCL, SBCL, and CLISP.

--- vcc/vcc.lisp
+++ vcc/vcc.lisp
@@ -1,10 +1,12 @@
 (in-package #:vacietis.vcc)
 
-#-ccl (error "VCC only supports Clozure Common Lisp for now.")
-
 (defun del (file)
   (when (probe-file file)
     (delete-file file)))
+
+(defun make-executable (path &key (toplevel #'main))
+  (del path)
+  (trivial-dump-core:save-executable path :init-function toplevel))
 
 (defun main ()
   (let* ((c-file    (second cl-user::*command-line-argument-list*))
@@ -14,21 +16,11 @@
                                   :use ())))
     (let ((*package* c-package))
       (load-c-file c-file))
-    (del "a.out")
-    (ccl:save-application
-     "a.out"
-     :toplevel-function (lambda ()
-                          (run-c-program c-package))
-     :error-handler     :quit
-     :prepend-kernel    t)))
+    (make-executable "a.out" :toplevel (lambda ()
+                                         (run-c-program c-package)))))
 
 (let ((vcc-out (merge-pathnames
                 "vcc"
                 #.(directory-namestring
                    (or *load-truename* *compile-file-truename*)))))
-  (del vcc-out)
-  (ccl:save-application
-   vcc-out
-   :toplevel-function #'main
-   :error-handler     :quit
-   :prepend-kernel    t))
+  (make-executable vcc-out))
