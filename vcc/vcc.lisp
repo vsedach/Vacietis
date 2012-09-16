823de3c09a56b683b82ad3eb8e5a2c206a3ab4c3
Brit Butler <redline6561@gmail.com>
2012-09-16 14:30:19 -0400

    Add portable getargv function.

--- vcc/vcc.lisp
+++ vcc/vcc.lisp
@@ -1,15 +1,24 @@
 (in-package #:vacietis.vcc)
 
+#-(or sbcl ccl clisp) (error "VCC only supports CCL, SBCL, and CLISP at this time.")
+
 (defun del (file)
   (when (probe-file file)
     (delete-file file)))
+
+(defun getargv ()
+  #+sbcl sb-ext:*posix-argv*
+  #+ccl ccl:*command-line-argument-list*
+  #+cmucl *command-line-strings*
+  #+clisp (ext:argv)
+  #+ecl (ext:command-args))
 
 (defun make-executable (path &key (toplevel #'main))
   (del path)
   (trivial-dump-core:save-executable path :init-function toplevel))
 
 (defun main ()
-  (let* ((c-file    (second cl-user::*command-line-argument-list*))
+  (let* ((c-file    (second (getargv)))
          (c-package (make-package (format nil "~A.PROGRAM"
                                           (string-upcase
                                            (file-namestring c-file)))
