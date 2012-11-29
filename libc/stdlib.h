50c1b82a9f906c270cd8cbc7a1fe7f7281ebad2f
Vladimir Sedach <vsedach@gmail.com>
2012-11-28 21:25:01 -0800

    Fixed reading of typedefs of structs

---
+++ libc/stdlib.h
@@ -0,0 +1,14 @@
+typedef struct {
+  int quot;
+  int rem;
+} div_t;
+
+typedef struct {
+  long quot;
+  long rem;
+} ldiv_t;
+
+/* typedef struct { */
+/*   long long int quot; */
+/*   long long int rem; */
+/* } lldiv_t; */
